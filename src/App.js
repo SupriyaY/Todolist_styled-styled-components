c7628aa3d8dc47fce9f0872a61cb49ae2f393aec
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-05-20 11:09:20 -0400

    added styled components to the todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './components/Button'
+import TodoList from './components/TodoList'
+import ClickMe from './components/PrettyButton'
+import PageHeader from './components/Nav'
+import NewButton from './components/BootstrapButton'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    let todoItems = [{author: 'Supriya', description: 'Styled-Components'},
+    {author: 'Destin', description: 'React Router'},
+    {author: 'Noah', description: 'Props & State'}
+  ]
+
+    return (
+      <div>
+        <PageHeader />
+<TodoList list={todoItems}  />
+        <Button />
+        <ClickMe /> 
+        <NewButton/>
+      </div>
+    )
+
+
+  }
+
 }
+
 
 export default App;
---
+++ src/components/BootstrapButton.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+
+
+
+
+
+class NewButton extends Component {
+
+  render() {
+    return (
+
+      <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+      </ButtonToolbar>
+
+
+
+    )
+  }
+}
+
+export default NewButton;
